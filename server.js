ddcf17f8c6da50bb605ea5d18a4754c621c2d8b5
Jon Sadowski <jwsadowski@gmail.com>
2023-01-17 17:09:49 -0600

    added dependencies and updated server.js

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const { Reaction } = require('./Models');
 const { Thought } = require('./Models');
 const { User } = require('./Models');
 
-const PORT = process.env.PORT || 3001
+const port = process.env.PORT || 3001
 const app = express();
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(routes);
+
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`);
+});
