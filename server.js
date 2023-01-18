0bc2017a7cdc5ea6ef5d7d07262e58cf7de601e9
Jon Sadowski <jwsadowski@gmail.com>
2023-01-18 13:48:59 -0600

    added something to the server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const routes = require('./routes');
 const { Reaction } = require('./Models');
 const { Thought } = require('./Models');
 const { User } = require('./Models');
