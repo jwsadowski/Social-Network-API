afb03e8dd9d216fc680bc595082da61ef15762e7
Jon Sadowski <jwsadowski@gmail.com>
2023-01-17 19:14:43 -0600

    working with subdocuments

--- Models/Thought.js
+++ Models/Thought.js
@@ -6,11 +6,7 @@ const thoughtSchema = new mongoose.Schema({
     thoughtText: { type : String, required: true},
     createdAt: { type: Date, default: Date.now },
     username: { type: String, required: true},
-    reactions: reactionSchema,
-    reactionId: { type: ObjectId, default: ObjectId},
-    reactionBody: { type: String, required: true, {280, 500} },
-    username: { type: String, required: true},
-    createdAt: {type: Date, default: Date.now }
+    reactions: reactionSchema
     
 }, {
     toJSON: {
