fc6202b7dad4c23167c3fc69d6e8cecc6e0a8f65
Jon Sadowski <jwsadowski@gmail.com>
2023-01-17 18:24:21 -0600

    updated models

--- Models/Thought.js
+++ Models/Thought.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+const { monitorEventLoopDelay } = require('perf_hooks');
 
 const thoughtSchema = new mongoose.Schema({
     thoughtText: { type : String, required: true},
     createdAt: { type: Date, default: Date.now },
     username: { type: String, required: true},
-    reactions: { [ ]}
+    reactions: { [ ] }
 })
+
+const Thought = mongoose.model('Thought', thoughtSchema)
+
+module.exports = Thought
--- Models/User.js
+++ Models/User.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, unique: true, required: true, trimmed: true },
-    email: { type: String, required: true, unique: true},
+    username: { 
+        type: String, 
+        unique: true, 
+        required: true, 
+        trimmed: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true
+    },
     thoughts: { [ _id: 'Thought']},
     friends: { [ _id: 'User']}
-})
+});
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
